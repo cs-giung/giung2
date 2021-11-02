010c2ddd40f7a135e8e36555a3110fb23cd3c9c4
cs-giung <giung@kaist.ac.kr>
2021-11-02 15:29:35 +0900

    update docstring.

--- giung2/evaluation/standard.py
+++ giung2/evaluation/standard.py
@@ -14,7 +14,14 @@ __all__ = [
 @torch.no_grad()
 def evaluate_acc(confidences: torch.Tensor,
                  true_labels: torch.Tensor) -> float:
+    """
+    Args:
+        confidences (Tensor): a tensor of shape [N, K] of predicted confidences.
+        true_labels (Tensor): a tensor of shape [N,] of ground truth labels.
 
+    Returns:
+        acc (float): average accuracy of predictions.
+    """
     acc = torch.max(
         confidences, dim=1
     )[1].eq(true_labels).float().mean().item()
@@ -25,7 +32,14 @@ def evaluate_acc(confidences: torch.Tensor,
 @torch.no_grad()
 def evaluate_nll(confidences: torch.Tensor,
                  true_labels: torch.Tensor) -> float:
-
+    """
+    Args:
+        confidences (Tensor): a tensor of shape [N, K] of predicted confidences.
+        true_labels (Tensor): a tensor of shape [N,] of ground truth labels.
+    
+    Returns:
+        nll (float): average negative-log-likelihood of predictions.
+    """
     nll = torch.nn.functional.nll_loss(
         torch.log(1e-12 + confidences), true_labels
     ).item()
@@ -36,7 +50,14 @@ def evaluate_nll(confidences: torch.Tensor,
 @torch.no_grad()
 def evaluate_bs(confidences: torch.Tensor,
                 true_labels: torch.Tensor) -> float:
-
+    """
+    Args:
+        confidences (Tensor): a tensor of shape [N, K] of predicted confidences.
+        true_labels (Tensor): a tensor of shape [N,] of ground truth labels.
+    
+    Returns:
+        bs (float): average Brier score of predictions.
+    """
     targets = torch.eye(
         confidences.size(1), device=confidences.device
     )[true_labels].long()
@@ -51,7 +72,14 @@ def evaluate_bs(confidences: torch.Tensor,
 @torch.no_grad()
 def get_optimal_temperature(confidences: torch.Tensor,
                             true_labels: torch.Tensor) -> float:
-
+    """
+    Args:
+        confidences (Tensor): a tensor of shape [N, K] of predicted confidences.
+        true_labels (Tensor): a tensor of shape [N,] of ground truth labels.
+    
+    Returns:
+        optimal_temperature (float): optimal value of temperature for given predictions.
+    """
     def obj(t):
         target = true_labels.cpu().numpy()
         return -np.log(
